89e8c85a84bcd4f81938034b31d45980105016b9
Eduardo <eduardooffside@gmail.com>
2021-05-04 16:30:12 -0300

    Basic static components

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/dududueedu" target="_blank" rel="noreferrer"> Eduardo da Silva </a></p>
+                <p className="text-light"><small><strong> Sistema - Spring React </strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/du.dudu.e.edu.__" target="_blank" rel="noreferrer"> @du.dudu.e.edu.__ </a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
